80924fcf4de9cac421121c55cbce8c6f55044a19
danielgsalas <danielgsalas@yahoo.com>
2016-07-10 10:08:09 -0700

    added helper method to determine Wi-Fi connectivity

--- app/src/main/java/com/appstoremarketresearch/android_wifilistview/controller/MainActivity.java
+++ app/src/main/java/com/appstoremarketresearch/android_wifilistview/controller/MainActivity.java
@@ -1,5 +1,8 @@
 package com.appstoremarketresearch.android_wifilistview.controller;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +12,23 @@ import com.appstoremarketresearch.android_wifilistview.R;
  * MainActivity
  */
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * isConnectedToWifi
+     */
+    public static boolean isConnectedToWifi(Context context) {
+
+        String service = Context.CONNECTIVITY_SERVICE;
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(service);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        if (isConnected) {
+            return activeNetwork.getType() == ConnectivityManager.TYPE_WIFI;
+        }
+
+        return false;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
